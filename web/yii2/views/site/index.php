acb4de7ba667afdcf9c732c38ca5803ec2f2080b
Mike Mozolin <mozolin@gmail.com>
2025-09-29 12:02:34 +0300

    it starts...

---
+++ web/yii2/views/site/index.php
@@ -0,0 +1,9 @@
+<?
+$this->title = 'Yii2 ESP OTBR Config';
+?>
+<div>
+	<?
+	//echo \Yii::$app->view->renderFile("@app/views/select-com-port.php");
+	echo \Yii::$app->view->renderFile("@app/views/get-otbr-proj-info.php");
+	?>
+</div>
