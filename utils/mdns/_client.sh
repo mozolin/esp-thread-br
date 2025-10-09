658ba33ff7e0dc11f046fbdb13abf5d69c26c39a
Mike Mozolin <mozolin@tillypad.ru>
2025-10-09 18:55:08 +0300

    mDNS Ubuntu

--- utils/mdns/_client.sh
+++ utils/mdns/_client.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-google-chrome --start-fullscreen "http://localhost:3000/"
+echo "========================================"
+echo "   mDNS Discovery Tool Launcher"
+echo "========================================"
+echo
+
+echo "Starting Chrome with mDNS interface..."
+google-chrome --start-fullscreen --disable-application-cache "http://localhost:3000/" &
+
+echo "Waiting for server to start..."
+sleep 2
+
+echo
+echo "If Chrome doesn't open automatically:"
+echo "1. Make sure Chrome is installed"
+echo "2. Check if it's in your PATH"
+echo "3. Or manually open: http://localhost:3000/"
+echo
+read -p "Press Enter to continue..."
